3d31b54e92df6b8c7b14d17a8aa63f565a860a25
Rade Pejanović <radepejanovic@gmail.com>
2024-04-17 19:04:33 +0200

    feat: Initialized Spring boot project

---
+++ pki-application/src/test/java/com/example/pki/pkiapplication/PkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pki.pkiapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
