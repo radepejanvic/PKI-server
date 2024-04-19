3dd32f25edcf6613c61bc910a02ba3fdfdd24c28
Rade Pejanović <radepejanovic@gmail.com>
2024-04-19 20:43:08 +0200

    feat: Added initial CRR model

---
+++ pki-application/src/main/java/com/example/pki/pkiapplication/service/CRRService.java
@@ -0,0 +1,7 @@
+package com.example.pki.pkiapplication.service;
+
+import com.example.pki.pkiapplication.model.CRR;
+
+public interface CRRService extends JpaService<CRR>{
+
+}
---
+++ pki-application/src/main/java/com/example/pki/pkiapplication/service/impl/CRRServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.pki.pkiapplication.service.impl;
+
+import com.example.pki.pkiapplication.model.CRR;
+import com.example.pki.pkiapplication.repository.CRRRepository;
+import com.example.pki.pkiapplication.service.CRRService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CRRServiceImpl implements CRRService {
+
+    @Autowired
+    private CRRRepository crrRepository;
+
+    @Override
+    public List<CRR> findAll() {
+        return crrRepository.findAll();
+    }
+
+    @Override
+    public CRR findOne(Long id) {
+        return crrRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public CRR save(CRR crr) {
+        return crrRepository.save(crr);
+    }
+
+    @Override
+    public void remove(CRR crr) {
+        crrRepository.delete(crr);
+    }
+}
