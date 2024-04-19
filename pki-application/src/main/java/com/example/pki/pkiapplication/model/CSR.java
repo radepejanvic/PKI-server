86b04c4274643ee87a9bb3955a76c9349f699ab2
Rade Pejanović <radepejanovic@gmail.com>
2024-04-19 17:18:10 +0200

    feat: Created initial models

---
+++ pki-application/src/main/java/com/example/pki/pkiapplication/model/CSR.java
@@ -0,0 +1,31 @@
+package com.example.pki.pkiapplication.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class CSR {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // base64 encoded public key
+    @Column(unique = true, nullable = false)
+    private String publicKey;
+
+    @Column(nullable = false)
+    private String commonName;
+
+    private String organization;
+
+    private String country;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private CertificateType template;
+
+    @Enumerated(EnumType.STRING)
+    private CertificateStatus status;
+}
