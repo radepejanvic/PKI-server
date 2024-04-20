7cbe42480624797e7c126732dc6fbf67ca0bd150
Rade Pejanović <radepejanovic@gmail.com>
2024-04-20 12:55:51 +0200

    fix: Fixed service annotations

--- pki-application/src/main/java/com/example/pki/pkiapplication/service/impl/CRRServiceImpl.java
+++ pki-application/src/main/java/com/example/pki/pkiapplication/service/impl/CRRServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.pki.pkiapplication.model.CRR;
 import com.example.pki.pkiapplication.repository.CRRRepository;
 import com.example.pki.pkiapplication.service.CRRService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CRRServiceImpl implements CRRService {
 
     @Autowired
--- pki-application/src/main/java/com/example/pki/pkiapplication/service/impl/CSRServiceImpl.java
+++ pki-application/src/main/java/com/example/pki/pkiapplication/service/impl/CSRServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.pki.pkiapplication.model.CSR;
 import com.example.pki.pkiapplication.repository.CSRRepository;
 import com.example.pki.pkiapplication.service.CSRService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CSRServiceImpl implements CSRService {
 
     @Autowired
