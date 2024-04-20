8aa05e2e10481d7d9afd03fb915d817b47654c8c
Rade Pejanović <radepejanovic@gmail.com>
2024-04-21 00:35:04 +0200

    feat: Addapted reader and writer for certificate only

--- pki-application/src/main/java/com/example/pki/pkiapplication/util/KeyStoreReader.java
+++ pki-application/src/main/java/com/example/pki/pkiapplication/util/KeyStoreReader.java
@@ -1,7 +1,11 @@
 package com.example.pki.pkiapplication.util;
 
+import com.example.pki.pkiapplication.model.Issuer;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.security.auth.x500.X500Principal;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -9,6 +13,7 @@ import java.io.IOException;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 @Component
 public class KeyStoreReader {
@@ -25,14 +30,13 @@ public class KeyStoreReader {
         }
     }
 
-    public Certificate readCertificate(String keyStoreFile, String keyStorePass, String alias) {
+    public X509Certificate readCertificate(String keyStoreFile, String keyStorePass, String alias) {
         try {
             BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
             keyStore.load(in, keyStorePass.toCharArray());
 
-            if(keyStore.isKeyEntry(alias)) {
-                Certificate cert = keyStore.getCertificate(alias);
-                return cert;
+            if(keyStore.isCertificateEntry(alias)) {
+                return (X509Certificate) keyStore.getCertificate(alias);
             }
         } catch (KeyStoreException e) {
             e.printStackTrace();
@@ -48,31 +52,11 @@ public class KeyStoreReader {
         return null;
     }
 
-    public PrivateKey readPrivateKey(String keyStoreFile, String keyStorePass, String alias, String pass) {
-        try {
-            BufferedInputStream in = new BufferedInputStream(new FileInputStream(keyStoreFile));
-            keyStore.load(in, keyStorePass.toCharArray());
+    public X500Name readIssuerX500Name(String keyStoreFile, String keyStorePass, String alias) {
+        X509Certificate x509Certificate = readCertificate(keyStoreFile, keyStorePass, alias);
 
-            if(keyStore.isKeyEntry(alias)) {
-                PrivateKey pk = (PrivateKey) keyStore.getKey(alias, pass.toCharArray());
-                return pk;
-            }
-        } catch (KeyStoreException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (CertificateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (UnrecoverableKeyException e) {
-            e.printStackTrace();
-        }
-        return null;
+        X500Principal x500Principal = x509Certificate.getIssuerX500Principal();
+        return new X500Name(x500Principal.getName());
     }
-
-
 
 }
