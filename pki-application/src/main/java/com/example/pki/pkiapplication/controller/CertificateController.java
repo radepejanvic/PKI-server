c81ad05f8c2dfb9351495ad5e7f128e3d5b02cb2
Rade Pejanović <radepejanovic@gmail.com>
2024-04-21 11:57:02 +0200

    feat: Adjusted endpoint to use certificate generating service

--- pki-application/src/main/java/com/example/pki/pkiapplication/controller/CertificateController.java
+++ pki-application/src/main/java/com/example/pki/pkiapplication/controller/CertificateController.java
@@ -1,9 +1,14 @@
 package com.example.pki.pkiapplication.controller;
 
+import com.example.pki.pkiapplication.dto.CSRDTO;
 import com.example.pki.pkiapplication.dto.CertificateDTO;
+import com.example.pki.pkiapplication.mapper.CSRDTOMapper;
 import com.example.pki.pkiapplication.mapper.CertificateDTOMapper;
+import com.example.pki.pkiapplication.model.CSR;
 import com.example.pki.pkiapplication.model.Certificate;
 import com.example.pki.pkiapplication.service.CertificateService;
+import com.example.pki.pkiapplication.service.impl.CertificateGeneratingServiceImpl;
+import com.sun.net.httpserver.HttpsServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,9 @@ public class CertificateController {
 
     @Autowired
     private CertificateService certificateService;
+
+    @Autowired
+    private CertificateGeneratingServiceImpl certificateGeneratingService;
 
     @GetMapping
     public ResponseEntity<?> getAll() {
@@ -37,6 +45,36 @@ public class CertificateController {
         if (certificate == null) { return new ResponseEntity<>(HttpStatus.NOT_FOUND); }
 
         return new ResponseEntity<>(CertificateDTOMapper.toDTO(certificate), HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<?> generateCertificate(@RequestBody CSRDTO dto) {
+
+        CSR csr = CSRDTOMapper.fromDTO(dto);
+
+        Certificate cert = null;
+
+        if (csr.getSubjectAlias().equals(csr.getIssuerAlias())) {
+            cert = certificateGeneratingService.generateRoot(csr);
+        } else {
+            cert = certificateGeneratingService.generateEE(csr);
+
+            Certificate issuerCert = certificateService.findByAlias(csr.getIssuerAlias());
+
+            if (issuerCert == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            cert.setIssuer(issuerCert);
+        }
+
+        cert.setAlias(csr.getSubjectAlias());
+        certificateService.save(cert);
+
+//        certificateGeneratingService.generateCA(csr);
+//        certificateGeneratingService.generateRoot(csr);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 
