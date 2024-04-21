f9c1c60b266ba6e0da01e92064ead349ad8d2858
Rade Pejanović <radepejanovic@gmail.com>
2024-04-21 11:46:34 +0200

    feat: Added find by alias

--- pki-application/src/main/java/com/example/pki/pkiapplication/service/CertificateService.java
+++ pki-application/src/main/java/com/example/pki/pkiapplication/service/CertificateService.java
@@ -1,8 +1,11 @@
 package com.example.pki.pkiapplication.service;
 
+import com.example.pki.pkiapplication.model.CSR;
 import com.example.pki.pkiapplication.model.Certificate;
 import org.springframework.stereotype.Service;
 
 public interface CertificateService extends JpaService<Certificate> {
+
+    Certificate findByAlias(String alias);
 
 }
